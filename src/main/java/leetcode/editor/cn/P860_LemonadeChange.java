8caad6ff4a921ea0f018f0a310c07cdf84156178
vincent <bupengwen@gmail.com>
2021-08-26 18:09:11 +0800

    找零钱

--- src/main/java/leetcode/editor/cn/P860_LemonadeChange.java
+++ src/main/java/leetcode/editor/cn/P860_LemonadeChange.java
@@ -1,8 +1,5 @@
 package leetcode.editor.cn;
 
-import java.util.LinkedList;
-
-
 //860-柠檬水找零
 public class P860_LemonadeChange {
 
@@ -24,34 +21,33 @@ public class P860_LemonadeChange {
         //复杂度O(N),用时10分钟
         public boolean lemonadeChange(int[] bills) {
             //维护2个数组,
-            LinkedList<Integer> five = new LinkedList<>();
-            LinkedList<Integer> ten = new LinkedList<>();
+            int five = 0, ten = 0;
+
             for (int i = 0; i < bills.length; i++) {
                 if (bills[i] == 5) {
-                    five.addLast(5);
+                    five++;
                 }else if (bills[i] == 10) {
                     //无法找零
-                    if (five.size() < 1) {
+                    if (five < 1) {
                         return false;
                     }
-                    ten.addLast(10);
-                    five.removeLast();
+                    ten++;
+                    five--;
+
                 }else if (bills[i] == 20) {
-                    if (five.isEmpty()) {
+                    if (five == 0) {
                         return false;
                     }
 
                     //有10元的
-                    if (ten.size() > 0) {
-                        ten.removeLast();
-                        five.removeLast();
+                    if (ten > 0) {
+                        ten--;
+                        five--;
                         continue;
                     }
                     //只有五元
-                    if (five.size() >= 3) {
-                        five.removeLast();
-                        five.removeLast();
-                        five.removeLast();
+                    if (five >= 3) {
+                        five -= 3;
                         continue;
                     }
                     //无法找零
