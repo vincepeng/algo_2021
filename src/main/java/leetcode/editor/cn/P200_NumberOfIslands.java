5e1c2a0ba20776e84455505e03ca38adfab965a8
bupengwen <bupengwen@gmail.com>
2021-09-15 12:25:54 +0800

    小岛

--- src/main/java/leetcode/editor/cn/P200_NumberOfIslands.java
+++ src/main/java/leetcode/editor/cn/P200_NumberOfIslands.java
@@ -5,57 +5,62 @@ public class P200_NumberOfIslands {
 
     public static void main(String[] args) {
 
-        Solution solution = new P200_NumberOfIslands().new Solution();
         char[][] grid = {//
                 {'1', '1', '1', '1', '0'},//
                 {'1', '1', '0', '1', '0'},//
                 {'1', '1', '0', '0', '0'},//
                 {'0', '0', '0', '0', '0'}};
+        char[][] grid2 = {//
+                {'1', '1', '0', '0', '0'},//
+                {'1', '1', '0', '0', '0'},//
+                {'0', '0', '1', '0', '0'},//
+                {'0', '0', '0', '1', '1'}//
+        };
+        Solution solution = new P200_NumberOfIslands().new Solution();
         System.out.println(solution.numIslands(grid));
+        System.out.println(solution.numIslands(grid2));
     }
 
 
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
 
-        private void dfs(char[][] grid, int i, int j) {
-            if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length) {
+        private void dfs(int i, int j, char[][] grid) {
+            int m = grid.length;
+            int n = grid[0].length;
+            if (i >= m || i < 0 || j >= n || j < 0) {
                 return;
             }
-            //可能为0或者2(遍历过)
-            if (grid[i][j] != '1') {
+            //访问过,或者为0;
+            if (grid[i][j] == '0' || grid[i][j] == '2') {
                 return;
             }
-
-            //染色,通过染色然后让无法连通的即使小岛
             grid[i][j] = '2';
-
-            dfs(grid, i, j - 1);
-            dfs(grid, i, j + 1);
-            dfs(grid, i - 1, j);
-            dfs(grid, i + 1, j);
-
+            dfs(i + 1, j, grid);
+            dfs(i - 1, j, grid);
+            dfs(i, j + 1, grid);
+            dfs(i, j - 1, grid);
         }
 
 
         public int numIslands(char[][] grid) {
-            if (grid.length == 0) {
-                return 0;
-            }
-            int height = grid.length;
-            int weight = grid[0].length;
-            int total = 0;
-            for (int i = 0; i < height; i++) {
-                for (int j = 0; j < weight; j++) {
+            //使用深度优先进行遍历;
+            int m = grid.length;
+            int n = grid[0].length;
+            int cnt = 0;
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
                     if (grid[i][j] == '1') {
-                        dfs(grid, i, j);
-                        total++;
+                        cnt++;
+                        dfs(i, j, grid);
                     }
+
                 }
             }
-            return total;
+            return cnt;
         }
-    }
-    //leetcode submit region end(Prohibit modification and deletion)
 
+    }
+
+    //leetcode submit region end(Prohibit modification and deletion)
 }
---
+++ src/main/java/leetcode/editor/cn/P200_NumberOfIslands2.java
@@ -0,0 +1,61 @@
+package leetcode.editor.cn;
+
+//200-岛屿数量
+public class P200_NumberOfIslands2 {
+
+    public static void main(String[] args) {
+
+        Solution solution = new P200_NumberOfIslands2().new Solution();
+        char[][] grid = {//
+                {'1', '1', '1', '1', '0'},//
+                {'1', '1', '0', '1', '0'},//
+                {'1', '1', '0', '0', '0'},//
+                {'0', '0', '0', '0', '0'}};
+        System.out.println(solution.numIslands(grid));
+    }
+
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+
+        private void dfs(char[][] grid, int i, int j) {
+            if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length) {
+                return;
+            }
+            //可能为0或者2(遍历过)
+            if (grid[i][j] != '1') {
+                return;
+            }
+
+            //染色,通过染色然后让无法连通的即使小岛
+            grid[i][j] = '2';
+
+            dfs(grid, i, j - 1);
+            dfs(grid, i, j + 1);
+            dfs(grid, i - 1, j);
+            dfs(grid, i + 1, j);
+
+        }
+
+
+        public int numIslands(char[][] grid) {
+            if (grid.length == 0) {
+                return 0;
+            }
+            int height = grid.length;
+            int weight = grid[0].length;
+            int total = 0;
+            for (int i = 0; i < height; i++) {
+                for (int j = 0; j < weight; j++) {
+                    if (grid[i][j] == '1') {
+                        dfs(grid, i, j);
+                        total++;
+                    }
+                }
+            }
+            return total;
+        }
+    }
+    //leetcode submit region end(Prohibit modification and deletion)
+
+}
