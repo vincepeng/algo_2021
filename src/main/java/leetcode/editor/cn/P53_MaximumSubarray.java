b6e7d1a9bf16611b9700900732da3e06328c599a
bupengwen <bupengwen@gmail.com>
2021-09-16 17:41:35 +0800

    继续用dp

--- src/main/java/leetcode/editor/cn/P53_MaximumSubarray.java
+++ src/main/java/leetcode/editor/cn/P53_MaximumSubarray.java
@@ -6,6 +6,28 @@ public class P53_MaximumSubarray {
     public static void main(String[] args) {
 
         Solution solution = new P53_MaximumSubarray().new Solution();
+        int[] a = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        assert solution.maxSubArray(a) == 6 : "失败了";
+    }
+
+
+    /**
+     * 复杂度O(N)
+     */
+    private int useDp(int[] nums) {
+        //动态规划
+        //状态dp[n]
+        //转移方程:dp[n]=dp[n-1]+nums[n]
+        //int max=Math.max(dp[n],i)
+        int next, pre = nums[0];
+        int max = 0;
+        for (int i = 1; i < nums.length; i++) {
+            next = pre > 0 ? pre + nums[i] : nums[i];
+            pre = next;
+            max = Math.max(next, max);
+        }
+        return max;
+
     }
 
 
@@ -14,6 +36,14 @@ public class P53_MaximumSubarray {
 
         public int maxSubArray(int[] nums) {
 
+            return useStrategy(nums);
+        }
+
+
+        /**
+         * O(n)
+         */
+        private int useStrategy(int[] nums) {
             int max = nums[0];
             int sum = 0;
 
@@ -31,6 +61,7 @@ public class P53_MaximumSubarray {
             return max;
         }
     }
+
     //leetcode submit region end(Prohibit modification and deletion)
 
 }
