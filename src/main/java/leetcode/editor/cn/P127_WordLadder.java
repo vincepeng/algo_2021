e59bb4bfa12acbaad48759f5a17e3cd8e48041dc
bupengwen <bupengwen@gmail.com>
2021-09-17 16:48:52 +0800

    单词接龙.

--- src/main/java/leetcode/editor/cn/P127_WordLadder.java
+++ src/main/java/leetcode/editor/cn/P127_WordLadder.java
@@ -1,26 +1,19 @@
 package leetcode.editor.cn;
 
 import java.time.Instant;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
-import java.util.Objects;
-import java.util.stream.Collectors;
+import java.util.Set;
 
 
 //127-单词接龙
 public class P127_WordLadder {
 
     public static void main(String[] args) {
-        Solution solution2 = new P127_WordLadder().new Solution();
-        String beginWord2 = "hit";
-        String endWord2 = "cog";
-        String[] wordList2 = {"hot", "dot", "dog", "lot", "log", "cog"};
-        System.out.println(solution2.ladderLength(beginWord2, endWord2, Arrays.asList(wordList2)));
+        test1();
+        test2();
 
 
         Solution solution = new P127_WordLadder().new Solution();
@@ -45,190 +38,99 @@ public class P127_WordLadder {
                 "red", "doc", "moe", "caw", "eel", "dix", "cub", "end", "gem", "off", "yew", "hug", "pop", "tub", "sgt", "lid", "pun", "ton", "sol", "din", "yup", "jab", "pea", "bug", "gag", "mil",
                 "jig", "hub", "low", "did", "tin", "get", "gte", "sox", "lei", "mig", "fig", "lon", "use", "ban", "flo", "nov", "jut", "bag", "mir", "sty", "lap", "two", "ins", "con", "ant", "net",
                 "tux", "ode", "stu", "mug", "cad", "nap", "gun", "fop", "tot", "sow", "sal", "sic", "ted", "wot", "del", "imp", "cob", "way", "ann", "tan", "mci", "job", "wet", "ism", "err", "him",
-                "all", "pad", "hah", "hie", "aim", "ike", "jed", "ego", "mac", "baa", "min", "com", "ill", "was", "cab", "ago", "ina", "big", "ilk", "gal", "tap", "duh", "ola", "ran", "lab", "top",
-                "gob", "hot", "ora", "tia", "kip", "han", "met", "hut", "she", "sac", "fed", "goo", "tee", "ell", "not", "act", "gil", "rut", "ala", "ape", "rig", "cid", "god", "duo", "lin", "aid",
-                "gel", "awl", "lag", "elf", "liz", "ref", "aha", "fib", "oho", "tho", "her", "nor", "ace", "adz", "fun", "ned", "coo", "win", "tao", "coy", "van", "man", "pit", "guy", "foe", "hid",
-                "mai", "sup", "jay", "hob", "mow", "jot", "are", "pol", "arc", "lax", "aft", "alb", "len", "air", "pug", "pox", "vow", "got", "meg", "zoe", "amp", "ale", "bud", "gee", "pin", "dun",
-                "pat", "ten", "mob"};
+                "all", "pad", "hah", "hie", "aim", "ike", "jed", "ego", "mac", "baa", "min", "com", "ill", "was", "cab", "ago", "ina", "big", "ilk", "gal", "tap", "duh", "ola", "ran", "lab", "top", "gob", "hot", "ora", "tia", "kip", "han", "met", "hut", "she", "sac", "fed", "goo", "tee", "ell", "not", "act", "gil", "rut", "ala", "ape", "rig", "cid", "god", "duo", "lin", "aid", "gel", "awl", "lag", "elf", "liz", "ref", "aha", "fib", "oho", "tho", "her", "nor", "ace", "adz", "fun", "ned", "coo", "win", "tao", "coy", "van", "man", "pit", "guy", "foe", "hid", "mai", "sup", "jay", "hob", "mow", "jot", "are", "pol", "arc", "lax", "aft", "alb", "len", "air", "pug", "pox", "vow", "got", "meg", "zoe", "amp", "ale", "bud", "gee", "pin", "dun", "pat", "ten", "mob"};
 
         Arrays.sort(wordList);
         long start = Instant.now().getEpochSecond();
-        System.out.println(solution.ladderLength(beginWord, endWord, Arrays.asList(wordList)));
+        //        System.out.println(solution.ladderLength(beginWord, endWord, Arrays.asList(wordList)));
         System.out.println("cost: " + (Instant.now().getEpochSecond() - start) + " s");
+    }
+
+
+    private static void test1() {
+        Solution solution2 = new P127_WordLadder().new Solution();
+        String beginWord2 = "hit";
+        String endWord2 = "cog";
+        //        String[] wordList2 = {"hot", "dot", "dog", "lot", "log"};
+        String[] wordList2 = {"hot", "dot", "dog", "lot", "log", "cog"};
+        System.out.println(solution2.ladderLength(beginWord2, endWord2, Arrays.asList(wordList2)));
+    }
+
+
+    private static void test2() {
+        Solution solution2 = new P127_WordLadder().new Solution();
+        String beginWord2 = "a";
+        String endWord2 = "c";
+        //        String[] wordList2 = {"hot", "dot", "dog", "lot", "log"};
+        String[] wordList2 = {"a", "b", "c"};
+        System.out.println(solution2.ladderLength(beginWord2, endWord2, Arrays.asList(wordList2)));
     }
 
 
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
 
-        private List<String> result = new ArrayList<>();
+        //广度优先
+        public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+            //1.wordList会多次遍历,然后这里使用set加快查询
+            Set<String> wordSets = new HashSet<>(wordList);
 
-        private Map<String, Integer> subStrResult = new HashMap<>();
-
-        private int maxLevel;
-
-
-        private void doFind(String endWord, LinkedList<String> wordsRecords, HashMap<Integer, Map<String, List<Pair>>> wordIndexMap) {
-            //终止条件;(单词用完)
-            String currentWord = wordsRecords.getLast();
-            Integer level = subStrResult.get(currentWord);
-            if (level != null && level <= wordsRecords.size() + 1) {
-                return;
+            int level = 1;
+            if (beginWord.equals(endWord)) {
+                return level;
             }
-
-            //当前当前满足条件了
-            if (currentWord.equals(endWord)) {
-
-                //因为要找最短的
-                if (wordsRecords.size() < result.size() || result.isEmpty()) {
-                    //                    System.out.println(wordsRecords);
-                    this.result = new ArrayList(wordsRecords);
-                }
-                return;
+            //不在字典中,则直接退出,无法转化
+            if (!wordSets.contains(endWord)) {
+                return 0;
             }
-            //已经遍历完成,但是没有找到
-            if (wordsRecords.size() == maxLevel) {
-                return;
-            }
-            //已经比当前的大了,直接忽略
-            if (!result.isEmpty() && wordsRecords.size() >= this.result.size()) {
+            LinkedList<String> queue = new LinkedList<>();
+            Set<String> visited = new HashSet<>();
+            queue.addLast(beginWord);
+            visited.add(beginWord);
+            //2.使用广度优先方法
+            while (queue.size() > 0) {
+                int levelSize = queue.size();
+                //3.由于是一层层的,所以需记录一下层的数量,
+                for (int index = 0; index < levelSize; index++) {
+                    String poll = queue.poll();
+                    char[] chars = poll.toCharArray();
+                    //3.由于单词可能会很长,如果按照单词的维度容易超过,因此这里使用字母来;(word.length*26)
+                    for (int i = 0; i < chars.length; i++) {
+                        char backup = chars[i];
+                        for (char j = 'a'; j <= 'z'; j++) {
+                            //与原先字母一样,则跳过
+                            if (backup == j) {
+                                continue;
+                            }else {
+                                chars[i] = j;
+                                String newStr = new String(chars);
+                                //不在单词表中,直接跳过
+                                if (!wordSets.contains(newStr)) {
+                                    continue;
+                                }
+                                if (newStr.equals(endWord)) {
+                                    return level + 1;
+                                }
+                                //,且不是重复访问,如果重复访问肯定就不是最短路径了
+                                if (!visited.contains(newStr)) {
+                                    //                                    System.out.println(newStr);
+                                    queue.addLast(newStr);
+                                    visited.add(newStr);
+                                }
 
-                return;
-            }
+                            }
 
-            while (wordsRecords.size() > 0) {
-                currentWord = wordsRecords.poll();
-                List<String> children = getChildren(currentWord, wordIndexMap, wordsRecords);
-                doSearchNextLevel(children, wordsRecords, wordIndexMap, endWord);
-
-            }
-
-
-            wordsRecords.addLast(currentWord);
-            subStrResult.put(currentWord, wordsRecords.size());
-        }
-
-
-        private void doSearchNextLevel(List<String> children, LinkedList<String> wordsRecords, HashMap<Integer, Map<String, List<Pair>>> wordIndexMap, String endWord) {
-            //            wordsRecords.addLast();
-            for (String child : children) {
-                if (child.equals(endWord)) {
-                    wordsRecords.addLast(child);
-                    System.out.println(wordsRecords);
-                    this.result = new ArrayList(wordsRecords);
-                    return;
-                }else {
-                    subStrResult.put(child, wordsRecords.size());
-                    //已经遍历完成,但是没有找到
-                    if (wordsRecords.size() == maxLevel) {
-                        return;
+                        }
+                        //恢复到上面一层;
+                        chars[i] = backup;
                     }
                 }
+                //上一层单词的遍历已经完成,这相当于到下一层了,则步数添加1个
+                level++;
             }
+            return 0;
         }
 
-
-        /**
-         * 因为单词的数量更短,所以以单词的为基数
-         */
-        private List<String> getChildren(String currentWord, HashMap<Integer, Map<String, List<Pair>>> wordIndexMap, LinkedList<String> wordsRecords) {
-            List<String> children = new ArrayList<>();
-            for (int i = 0; i < currentWord.length(); i++) {
-                String subStringWithoutI = getSubStringWithoutI(i, currentWord);
-                Map<String, List<Pair>> stringListMap = wordIndexMap.get(i);
-                List<Pair> pairs = stringListMap.get(subStringWithoutI);
-                //没有子类
-                if (pairs == null || pairs.isEmpty()) {
-                    continue;
-                }
-                //找到那些没有用过的子类(复杂度 O(N)
-                HashSet<String> tempSet = new HashSet<>(wordsRecords);
-                List<String> collect = pairs.stream().filter(pair -> !tempSet.contains(pair.getWord())).map(Pair::getWord).collect(Collectors.toList());
-                children.addAll(collect);
-
-            }
-            return children;
-        }
-
-
-        private String getSubStringWithoutI(int finalI, String s) {
-            if (finalI == 0) {
-                return s.substring(finalI + 1);
-            }else if (finalI == s.length() - 1) {
-                return s.substring(0, finalI);
-            }else {
-                return s.substring(0, finalI) + s.substring(finalI + 1);
-            }
-        }
-
-
-        public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-            return useRecursion(beginWord, endWord, wordList);
-        }
-
-
-        private int useRecursion(String beginWord, String endWord, List<String> wordList) {
-
-            this.maxLevel = wordList.size();
-            HashMap<Integer, Map<String, List<Pair>>> wordMap = new HashMap<>();
-            for (int i = 0; i < beginWord.length(); i++) {
-                int finalI = i;
-                Map<String, List<Pair>> groupList = wordList.stream().map(s -> {
-                    String subStringWithoutI = getSubStringWithoutI(finalI, s);
-                    Pair pair = new Pair(subStringWithoutI, s);
-                    return pair;
-                }).collect(Collectors.groupingBy(Pair::getSubString));
-                wordMap.put(i, groupList);
-            }
-
-
-            LinkedList<String> wordsRecords = new LinkedList<>();
-            wordsRecords.addLast(beginWord);
-            doFind(endWord, wordsRecords, wordMap);
-
-            return result.size();
-        }
-
-
-        class Pair {
-
-            private String subString;
-
-            private String word;
-
-
-            public Pair(String subString, String word) {
-                this.subString = subString;
-                this.word = word;
-            }
-
-
-            @Override
-            public boolean equals(Object o) {
-                if (this == o)
-                    return true;
-                if (o == null || getClass() != o.getClass())
-                    return false;
-                Pair pair = (Pair) o;
-                return word.equals(pair.word);
-            }
-
-
-            public String getSubString() {
-                return subString;
-            }
-
-
-            public String getWord() {
-                return word;
-            }
-
-
-            @Override
-            public int hashCode() {
-                return Objects.hash(word);
-            }
-        }
     }
     //leetcode submit region end(Prohibit modification and deletion)
 
